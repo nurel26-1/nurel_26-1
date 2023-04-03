eec01ac8075064a7ffd7214705c60d98f0309143
nurel26-1 <nlomurzakov@gmail.com>
2023-04-03 17:32:06 +0600

    hw6

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ def register_view(request):
             if form.cleaned_data.get('password1') == form.cleaned_data.get('password2'):
                 User.objects.create_user(
                     username=form.cleaned_data.get('username'),
-                    password1=form.cleaned_data.get('password1')
+                    password=form.cleaned_data.get('password1')
                 )
                 return redirect('/users/register/')
             else:
